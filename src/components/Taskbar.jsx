96b56bbe384bd8b3836bf1aee338dc701477c784
Thomas chacko <165127786+thomas-stratagile@users.noreply.github.com>
2025-10-10 18:20:36 +0530

    date format

--- src/components/Taskbar.jsx
+++ src/components/Taskbar.jsx
@@ -45,7 +45,8 @@ const Taskbar = ({ windows, activeWindow, onRestoreWindow }) => {
     const formatMobileDate = (date) => {
         const options = {
             day: '2-digit',
-            month: '2-digit'
+            month: '2-digit',
+            year: '2-digit'
         }
         return date.toLocaleDateString('en-GB', options)
     }
